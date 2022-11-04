79afafb6a4e1b8b6c083c3d6772574d010f1e736
alex <alex@equipmoz.org>
2022-11-04 14:12:17 +0200

    fibbonaci challenge'

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('hello js')
+const fibonacci = length => {
+  let fiboSequence = [0, 1];
+  for (let i = 0; i <= length - 3; i++) {
+    let nextValue = fiboSequence[fiboSequence.length - 1] + fiboSequence[fiboSequence.length - 2];
+    fiboSequence.push(nextValue);
+  }
+  console.log('fibbonacci', fiboSequence)
+}
+
+fibonacci(10);
